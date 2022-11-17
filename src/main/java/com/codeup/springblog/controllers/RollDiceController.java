1bef1937ed111e365d8f80ec589b00951ae16f8d
JazMac <jazmine.n.hudson@gmail.com>
2022-11-17 17:57:07 -0600

    continue to work on controllers

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -3,19 +3,18 @@ package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class RollDiceController {
 
-    @GetMapping("/roll-dice")
-    @ResponseBody
-    public String intro(){
-        return "roll-Dice";
+    @RequestMapping("/roll-dice")
+    public String rollDice(){
+        return "rollDice";
     }
 
-    @GetMapping("/roll-dice/{n}")
-    @ResponseBody
+    @GetMapping("/{n}")
     public int rollGuess(@PathVariable int n){
 //        double rand = (Math.random() + 1),
         return n;
