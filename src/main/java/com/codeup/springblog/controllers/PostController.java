4a440365f7591ddb8e315e22ae97f633452c66b4
JazMac <jazmine.n.hudson@gmail.com>
2022-11-29 09:05:56 -0600

    relationship exercieses complete

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,27 +2,51 @@ package com.codeup.springblog.controllers;
 
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/posts") /// <-- will at to the @GetMapping's below
-public class PostController {
+public class PostController{
 
     private final PostRepository postDao;
+    private final UserRepository userDao;
 
-    public PostController(PostRepository postDao){
+    public PostController(PostRepository postDao, UserRepository userDao){
         this.postDao = postDao;
+        this.userDao = userDao;
     }
+
+//    @GetMapping("/user-login")
+//    public String createUser(){
+//        return "posts/user-login";
+//    }
+//
+//    @PostMapping("/user-login")
+//    public String addUser(@RequestParam(name="username") String username,
+//                          @RequestParam(name="email") String email,
+//                          @RequestParam(name="password")String password){
+//        User user = new User(username, email, password);
+//        userDao.save(user);
+//        return "redirect:/posts/index";
+//    }
+
 
     @GetMapping
     public String allPosts(Model model) {
         List <Post> allPosts = postDao.findAll();
+//        User user = userDao.getById(1);
+//        Post post = new Post();
+//        post.setUser(user);
+//        post.setTitle("Hi Everyone!");
+//        post.setBody("How is everyone?");
+//        postDao.save(post);
         model.addAttribute("allPosts", allPosts);
         return "posts/index";
     }
@@ -43,8 +67,11 @@ public class PostController {
     }
 
     @PostMapping("/create")
-    public String addPost(@RequestParam(name="title") String title, @RequestParam(name="body") String body){
-        Post post = new Post(title, body);
+    public String addPost(@RequestParam(name="title") String title,
+                          @RequestParam(name="body") String body){
+
+        User user = userDao.getById(2L);
+        Post post = new Post(title, body, user);
         postDao.save(post);
         return "redirect:/posts";
     }
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -15,6 +15,17 @@ public class Post {
     @Column(nullable = false, length = 1000)
     private String body;
 
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public String getTitle() {
         return title;
@@ -45,4 +56,15 @@ public class Post {
         this.body = body;
     }
 
+    public Post(String title, String body, User user) {
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 }
